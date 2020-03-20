da58db01c5d9ec5b342afa9ace2a50fa592ae29e
papiamento <1024836+papiamento@users.noreply.github.com>
2020-03-20 12:16:55 -0400

    remove social cname

--- aws_route53_record.tf
+++ aws_route53_record.tf
@@ -10,19 +10,6 @@ resource "aws_route53_record" "live-michel-dance" {
   }
 }
 
-resource "aws_route53_record" "social-michel-dance" {
-  zone_id = "Z1IUD2R4RK3VET"
-  name    = "social.michel.dance"
-  type    = "CNAME"
-
-  alias {
-    name                   = aws_s3_bucket.live-michel-dance.website_endpoint
-    zone_id                = aws_s3_bucket.live-michel-dance.hosted_zone_id
-    evaluate_target_health = false
-  }
-}
-
-
 output "website_endpoint" {
   value = aws_s3_bucket.live-michel-dance.website_endpoint
 }
