75cd5ccc8ec07ff466cf72ee3161a0c1032bffb6
papiamento <1024836+papiamento@users.noreply.github.com>
2020-03-20 11:13:38 -0400

    trying alias name same as bucket name

--- aws_route53_record.tf
+++ aws_route53_record.tf
@@ -4,8 +4,8 @@ resource "aws_route53_record" "live-michel-dance" {
   type    = "A"
 
   alias {
-    name                   = aws_s3_bucket.live-michel-dance.bucket_domain_name
-    zone_id                = "Z1IUD2R4RK3VET"
+    name                   = "live.michel.dance"
+    zone_id                = "aws_s3_bucket.live-michel-dance.hosted_zone_id"
     evaluate_target_health = false
   }
 }
