9fd0e34ab26c456de999b45bb352c5f5c9b054a7
papiamento <1024836+papiamento@users.noreply.github.com>
2020-03-20 11:17:32 -0400

    what is a website endpoint

--- aws_route53_record.tf
+++ aws_route53_record.tf
@@ -4,8 +4,12 @@ resource "aws_route53_record" "live-michel-dance" {
   type    = "A"
 
   alias {
-    name                   = "live.michel.dance"
+    name                   = aws_s3_bucket.live-michel-dance.website_endpoint
     zone_id                = aws_s3_bucket.live-michel-dance.hosted_zone_id
     evaluate_target_health = false
   }
+}
+
+output "website_endpoint" {
+  value = aws_s3_bucket.live-michel-dance.website_endpoint
 }
